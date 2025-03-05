07a437911caa45b91626cc2e5ab7a71bf5cc73c8
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-05 15:40:04 +0500

    Remove sign-up route, update MongoDB connection error handling, and add user model with bcrypt dependency

--- app/api/sign-up/route.js
+++
@@ -1,7 +0,0 @@
-import connectDB from "@/lib/db";
-import { NextResponse as res } from "next/server";
-
-export const GET = async () => {
-  connectDB()
-  return res.json({ message: "Hello World" });
-}
